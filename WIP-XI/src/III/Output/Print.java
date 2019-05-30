82a4bb3feafe19564c5f080d5e9c934e1be4240b
thana12 <numkeang.k@gmail.com>
2019-05-30 11:37:30 +0700

    [ADD] New Project
    Java/30.05

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,23 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("สวัสดีค่ะชื่อน้ำ");
+        
+        System.out.print("อยู่รสมันม่วง \n");
+        
+        // \n <-- new line (ชี้นบรรทัดใหม่)
+        System.out.print("เพื่อนคนถัดไปชื่อ กิจ \n");
+        
+        System.out.print("ส่วนเพื่อนคนถัดไปอีกชื่อ ไอซ์ ");
+        
+    }
+}
---
+++ WIP-XI/src/VI/Input/_0_Scanner.java
@@ -0,0 +1,34 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package VI.Input;
+
+import java.util.Scanner;
+
+/**
+ * อย่าลืม!!!! IMPORT PACKAGE
+ */
+
+
+
+public class _0_Scanner {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter x : ");
+
+        int x = sc.nextInt();
+        
+        System.out.print("Enter y : ");
+
+        int y = sc.nextInt();
+        
+        System.out.println("X : " + x);
+        System.out.println("Y : " + y);
+        
+        System.out.println("X+Y : " + (x+y));
+        
+    }
+}
